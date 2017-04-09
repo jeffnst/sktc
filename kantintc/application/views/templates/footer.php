58041b3d1447281281a578f6e986cfbfa7c532dd
rahmat nazali s <rahmatnazali95@gmail.com>
2017-04-09 18:19:36 +0700

    Upload web application

---
+++ kantintc/application/views/templates/footer.php
@@ -0,0 +1,28 @@
+        <section id="contact" name="contact"></section>
+        <div id="footerwrap">
+            <div class="container">
+                <div class="col-lg-5">
+                    <h3>Kantin Teknik Informatika ITS</h3>
+                    <p>
+                        Jl. Teknik Kimia, Sukolilo, Surabaya <br>
+                        Telepon: 0812345678 &nbsp; | &nbsp; Facebook: Kantin TC <br> Twitter: @kantinTC &nbsp; | &nbsp; Instagram: @kantinTC
+                    </p>
+                </div>
+
+                <div class="col-lg-7">
+                    <p style="float:right; margin-top:10%">Dibuat dengan penuh cinta oleh&nbsp;<a href="https://www.facebook.com/rahmat.nazali" target="_blank">WANDS</a></img></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+        <script>
+            $('.carousel').carousel({
+                interval: 3500
+            })
+        </script>
+    </body>
+</html>
